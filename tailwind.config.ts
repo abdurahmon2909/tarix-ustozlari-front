7668cb4ddaca14c7324903a011496d0837e24e55
abdurahmon2909 <abdurahmon29091996@gmail.com>
2026-05-10 14:19:26 +0500

    fix tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,25 +1,13 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
     "./index.html",
-
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
 
   theme: {
-    extend: {
-      colors: {
-        primary: "#111827",
-
-        secondary: "#1F2937",
-
-        accent: "#2563EB",
-      },
-    },
+    extend: {},
   },
 
   plugins: [],
 };
-
-export default config;
