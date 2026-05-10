28e9dae48eac897bfff3a9c617c5088b13acd3bd
abdurahmon2909 <abdurahmon29091996@gmail.com>
2026-05-10 15:47:56 +0500

    add premium homepage

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -1,14 +1,30 @@
-import { Outlet } from "react-router-dom";
+import BottomNav from "./BottomNav";
 
-import BottomNav from "./BottomNavbar";
-import PageContainer from "./PageContainer";
+interface Props {
+  children: React.ReactNode;
+}
 
-export default function AppShell() {
+export default function AppShell({
+  children,
+}: Props) {
   return (
-    <div className="bg-[#071120] min-h-screen">
-      <PageContainer>
-        <Outlet />
-      </PageContainer>
+    <div
+      className="
+      min-h-screen
+      bg-[#050B16]
+      text-white
+      pb-28
+    "
+    >
+      <main
+        className="
+        max-w-md
+        mx-auto
+        px-4
+      "
+      >
+        {children}
+      </main>
 
       <BottomNav />
     </div>
